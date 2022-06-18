90f8e8ef74c5e11f86bc0c7eb28ea567728099fa
saeed <saeedkariminia542@gmail.com>
2022-06-18 10:25:28 +0430

    change trailer banner

--- Components/trailer/home-banner.js
+++ Components/trailer/home-banner.js
@@ -3,7 +3,7 @@ import {Fragment} from "react";
 const HomeBanner = () => {
     return (
       <Fragment>
-          <div className="alternateHomeBanner" style={{backgroundImage: `url(../assets/images/trailer-redesign-image.jpg)`,backgroundPosition: "center",}}>
+          <div className="alternateHomeBanner" style={{backgroundImage: `url(../assets/images/trailerMain-banner.jpg)`,backgroundPosition: "center",}}>
           </div>
       </Fragment>
     );
